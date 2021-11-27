c5a014a18931492ea86942ede56e77c729639cc2
Robin <robin-kraft@live.de>
2021-11-27 11:27:56 +0100

    feat; füge Grundgerüst hinzu

--- go.mod
+++ go.mod
@@ -8,6 +8,10 @@ require (
 )
 
 require (
+	github.com/KnutZuidema/golio v0.0.0-20211124075333-53eb035e56ed // indirect
 	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	golang.org/x/crypto v0.0.0-20181030102418-4d3f4d9ffa16 // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
 )
--- main.go
+++ main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"nemesisbot/config"
 	"nemesisbot/database"
 	"os"
+	"time"
+
+	"github.com/KnutZuidema/golio"
+	"github.com/KnutZuidema/golio/api"
+	"github.com/KnutZuidema/golio/riot/lol"
 )
 
 func main() {
+	config.ReadConfig()
+
+	setupDatabase()
+	setupGalio()
+
+}
+func setupDatabase() {
 	databasefolders := "sqlite/"
 	databasename := "database.db"
 	os.Mkdir(databasefolders, os.ModePerm)
@@ -19,28 +33,58 @@ func main() {
 
 	database.AddEntry("foo", "bar")
 
-	//config.ReadConfig()
-	//dg, err := discordgo.New("Bot " + config.DiscordToken)
-	//if err != nil {
-	//	fmt.Println("error creating Discord session,", err)
-	//	return
-	//}
-	//
-	////eventhandler.Init()
-	////dg.AddHandler(eventhandler.VoiceChannelCreate)
-	//
-	//dg.Identify.Intents = discordgo.IntentsAll
-	//
-	//err = dg.Open()
-	//if err != nil {
-	//	fmt.Println("error opening connection,", err)
-	//	return
-	//}
-	//
-	//fmt.Println("Bot is now running.  Press CTRL-C to exit.")
-	//sc := make(chan os.Signal, 1)
-	//signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	//<-sc
-	//
-	//dg.Close()
 }
+
+func setupGalio() {
+	client := golio.NewClient(config.RiotToken,
+		golio.WithRegion(api.RegionEuropeWest))
+	summoner, _ := client.Riot.LoL.Summoner.GetByName("DreiAugenFlappe")
+	matches, _ := client.Riot.LoL.Match.List(summoner.PUUID, 0, 5)
+
+	for _, matchName := range matches {
+
+		match, _ := client.Riot.LoL.Match.Get(matchName)
+		fmt.Print(time.Unix(match.Info.GameStartTimestamp/1000, 0))
+		fmt.Print(": ")
+		participants := match.Info.Participants
+
+		findNemesis(participants)
+
+		fmt.Print("\n")
+	}
+}
+
+func findNemesis(participants []*lol.Participant) int {
+	for id, participant := range participants {
+		fmt.Print(id)
+		fmt.Print(" ")
+		fmt.Print(participant.ChampionName + ", ")
+	}
+	return -1
+
+}
+
+//config.ReadConfig()
+//dg, err := discordgo.New("Bot " + config.DiscordToken)
+//if err != nil {
+//	fmt.Println("error creating Discord session,", err)
+//	return
+//}
+//
+////eventhandler.Init()
+////dg.AddHandler(eventhandler.VoiceChannelCreate)
+//
+//dg.Identify.Intents = discordgo.IntentsAll
+//
+//err = dg.Open()
+//if err != nil {
+//	fmt.Println("error opening connection,", err)
+//	return
+//}
+//
+//fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+//sc := make(chan os.Signal, 1)
+//signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+//<-sc
+//
+//dg.Close()
